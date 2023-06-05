9fe7b5a97b8ae7f7891224e5769176aec346c447
ujjaneaish <132292694+ujjaneaish@users.noreply.github.com>
2023-06-05 13:24:41 -0700

    Update toronto-gke-service-account.tf
    hard coded the project id

--- 3. GKE-Cluster-Toronto/toronto-gke-service-account.tf
+++ 3. GKE-Cluster-Toronto/toronto-gke-service-account.tf
@@ -5,49 +5,49 @@ resource "google_service_account" "toronto_gke_sa" {
 
 
 resource "google_project_iam_binding" "bind_toronto_gke_sa_to_artifact_registry" {
-  project = var.gcp-project-id
+  project = "pd-sandbox-project-388413"
   role    = "roles/artifactregistry.writer"
   members = ["serviceAccount:${google_service_account.toronto_gke_sa.email}"]
 }
 
 resource "google_project_iam_binding" "toronto_gke_sa_binding" {
-  project = var.gcp-project-id
+  project = "pd-sandbox-project-388413"
   role    = "roles/compute.viewer"
   members = ["serviceAccount:${google_service_account.toronto_gke_sa.email}"]
 }
 
 resource "google_project_iam_binding" "toronto_gke_sa_binding_securityAdmin" {
-  project = var.gcp-project-id
+  project = "pd-sandbox-project-388413"
   role    = "roles/compute.securityAdmin"
   members = ["serviceAccount:${google_service_account.toronto_gke_sa.email}"]
 }
 
 resource "google_project_iam_binding" "toronto_gke_sa_binding_clusterAdmin" {
-  project = var.gcp-project-id
+  project = "pd-sandbox-project-388413"
   role    = "roles/container.clusterAdmin"
   members = ["serviceAccount:${google_service_account.toronto_gke_sa.email}"]
 }
 
 resource "google_project_iam_binding" "toronto_gke_sa_binding_developer" {
-  project = var.gcp-project-id
+  project = "pd-sandbox-project-388413"
   role    = "roles/container.developer"
   members = ["serviceAccount:${google_service_account.toronto_gke_sa.email}"]
 }
 
 resource "google_project_iam_binding" "toronto_gke_sa_binding_saAdmin" {
-  project = var.gcp-project-id
+  project = "pd-sandbox-project-388413"
   role    = "roles/iam.serviceAccountAdmin"
   members = ["serviceAccount:${google_service_account.toronto_gke_sa.email}"]
 }
 
 resource "google_project_iam_binding" "toronto_gke_sa_binding_saUser" {
-  project = var.gcp-project-id
+  project = "pd-sandbox-project-388413"
   role    = "roles/iam.serviceAccountUser"
   members = ["serviceAccount:${google_service_account.toronto_gke_sa.email}"]
 }
 
 resource "google_project_iam_binding" "toronto_gke_sa_binding_projectIamAdmin" {
-  project = var.gcp-project-id
+  project = "pd-sandbox-project-388413"
   role    = "roles/resourcemanager.projectIamAdmin"
   members = ["serviceAccount:${google_service_account.toronto_gke_sa.email}"]
 }
@@ -55,6 +55,6 @@ resource "google_project_iam_binding" "toronto_gke_sa_binding_projectIamAdmin" {
 
 resource "google_project_iam_binding" "secrets_manager_access" {
   role    = "roles/secretmanager.secretAccessor"
-  project = var.gcp-project-id
+  project = "pd-sandbox-project-388413"
   members = ["serviceAccount:${google_service_account.toronto_gke_sa.email}"]
 }
